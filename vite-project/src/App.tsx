a128e3967f3f6671a7d0b036cbbbf87461559e62
atere21 <you@tosinateret@gmail.com>
2023-09-05 10:28:11 -0700

    todo-list

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"; // Import React if it's not already imported
+import { TodoList } from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
